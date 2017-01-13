d9bde01e3a87699fe1a1789cfcc810b541218880
zoltan.csermely <zoltan.csermely@SWS00196>
2017-01-13 13:20:14 +0100

    # Added: Add method

--- Sample1/Sample1/Calc.cs
+++ Sample1/Sample1/Calc.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Sample1
 {
-    class Calc
+    public class Calc
     {
+        public double Add(double x, double y)
+        {
+            
+        }
     }
 }
